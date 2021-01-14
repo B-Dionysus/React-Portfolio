c6189a9494edbfe019e5490268667779032a1fc5
Benjamin Dionysus <benjamin@sixbynine.com>
2021-01-14 14:30:02 -0600

    Resume loads dynamically from database, as was always intended

--- client/src/components/main/AboutLeft.js
+++ client/src/components/main/AboutLeft.js
@@ -1,7 +1,21 @@
 import HeroImage from "./HeroImage";
-// This component sits just ot the lef tof the main text content on the main page, and holds the 
+import {useEffect} from "react";
+import {getPortfolio} from "../../utils/API"
+
+// This component sits just ot the left of the main text content on the main page, and holds the 
 // hero image and my contact information
+let resumeLink;
  function AboutLeft() {
+    useEffect(()=>{
+        getPortfolio(1)
+        .then(res=>{
+            if(res){
+                resumeLink=res.data[2].externalLink;
+            }
+        });
+    },[])
+
+
    return (
     <div className="col-12 col-md-4" id="main-left">
         <HeroImage />
@@ -12,7 +26,7 @@ import HeroImage from "./HeroImage";
                 <li className="list-group list-group-flush">773-209-2241</li>
                 <a rel="noreferrer" className="list-group list-group-flush col-12" target="_BLANK" href="https://www.linkedin.com/in/benjamin-dionysus-10a638182/">Linked In</a>
                 <a rel="noreferrer" className="list-group list-group-flush col-12" target="_BLANK" href="https://github.com/B-Dionysus">Github</a>
-                <a rel="noreferrer" className="list-group list-group-flush col-12" target="_BLANK" href="https://drive.google.com/file/d/1zQEYziV1RfxfWnq_xyS5lmKEJibf82O0/view?usp=sharing">Resume (PDF)</a>
+                <a rel="noreferrer" className="list-group list-group-flush col-12" target="_BLANK" href={resumeLink}>Resume (PDF)</a>
             </ul>
         </div>
     </div>
